1dd6ca528a6e3ab8eb80179dc4b6a8d729f03812
Ally Murray <allymurray88@gmail.com>
2019-03-18 20:13:48 +0000

    Open tweet in new tab

--- src/components/tweetQuote.js
+++ src/components/tweetQuote.js
@@ -6,7 +6,12 @@ const tweetUrl = "https://twitter.com/intent/tweet?hashtags=quote&url=/&text=";
 export default class TweetButton extends Component {
   render() {
     return (
-      <a id="tweet-quote" href={tweetUrl + this.props.tweet}>
+      <a
+        id="tweet-quote"
+        href={tweetUrl + this.props.tweet}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fab fa-twitter" onClick={this.props.onClick} />
       </a>
     );
