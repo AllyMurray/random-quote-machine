5e261d70c8aaf76b9e078839f468e06b878734d5
Ally Murray <allymurray88@gmail.com>
2019-03-20 09:34:26 +0000

    Tidy up layout

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import "./quote.css";
 
 export default class Quote extends Component {
   render() {
     return (
       <div>
-        <i className="fas fa-quote-left" />
-        <p id="text">{this.props.text}</p>
-        <p id="author">{this.props.author ? `-${this.props.author}` : ""}</p>
+        <p id="text">
+          <i className="fas fa-quote-left" />
+          {this.props.text}
+        </p>
+        <p id="author" className="author">
+          {this.props.author ? `-${this.props.author}` : ""}
+        </p>
       </div>
     );
   }
