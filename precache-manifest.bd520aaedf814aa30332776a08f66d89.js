b8bec5da4144fa3ee51ccca02c9cd6cb9b484241
Ally Murray <allymurray88@gmail.com>
2019-10-30 11:35:42 +0000

    Updates

---
+++ precache-manifest.bd520aaedf814aa30332776a08f66d89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc88e813dcf2fcb54ffb868ed86a54c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "96b637ceddd8f3420bc1",
+    "url": "/random-quote-machine/static/css/main.5a71ac8b.chunk.css"
+  },
+  {
+    "revision": "5efcfd60b820a8943931",
+    "url": "/random-quote-machine/static/js/2.bdf1bda4.chunk.js"
+  },
+  {
+    "revision": "96b637ceddd8f3420bc1",
+    "url": "/random-quote-machine/static/js/main.5cc80489.chunk.js"
+  },
+  {
+    "revision": "a1217defecffe873141b",
+    "url": "/random-quote-machine/static/js/runtime-main.6d700caf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-quote-machine/precache-manifest.0d44c6d49b4dd2b2581085e1149fe1b5.js"
+  "/random-quote-machine/precache-manifest.bd520aaedf814aa30332776a08f66d89.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/random-quote-machine/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/random-quote-machine/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
